127e17c21d98c1cee3df6d747aadd4a049fe5959
Varg <va@pm.me>
2024-09-13 18:32:56 +0200

    Fix release pipeline

--- cross_build/build_linux64.sh
+++ cross_build/build_linux64.sh
@@ -9,7 +9,7 @@ SCRIPT=cross_build/build_AppImage.sh
 
 SSH_DIR="$HOME/.ssh"
 HOST_VOLUMES="-v $SSH_DIR:/home/$(id -un)/.ssh"
-USER_IDS=(-e BUILDER_UID="$( id -u )" -e BUILDER_GID="$( id -g )" -e BUILDER_USER="$( id -un )" -e BUILDER_GROUP="$( id -gn )")
+USER_IDS="-e BUILDER_UID=$( id -u ) -e BUILDER_GID=$( id -g ) -e BUILDER_USER=$( id -un ) -e BUILDER_GROUP=$( id -gn )"
 # Allow usage of fuse
 DOCKER_OPTS="--cap-add SYS_ADMIN --device /dev/fuse --security-opt apparmor:unconfined"
 
@@ -17,5 +17,5 @@ docker run --rm -ti \
   -v "$(pwd)":/work \
   $HOST_VOLUMES \
   $DOCKER_OPTS \
-  "${USER_IDS[@]}" \
+  $USER_IDS \
   $IMAGE "/work/$SCRIPT"
